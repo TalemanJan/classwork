ed130fe5d086c3be7c0bf4c89a7fc286cb1e4ac1
talemanjan <talemanaptech@gmail.com>
2023-03-27 18:10:49 +0500

    added js

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
 function studentdetails(name,studentid,clas){
 
-return `your name is: ${name}your ID: ${studentid}your class: ${clas}`
+return `your name is: ${name}\nyour ID: ${studentid}\nyour class: ${clas}`
 
 }
 
 console.log(studentdetails("taleman",1243,12))
+
+
+
+function getobtainmarks(eng,math,urdu,sci,comp){
+
+  return `eng: ${eng}\nmath: ${math}\nurdu: ${urdu}\nsci: ${sci}\ncomp: ${comp}\n `;
+
+
+}
+
+console.log(getobtainmarks(45,65,45,77,34,87))
+
+
+
+
+
+
+
+
+
